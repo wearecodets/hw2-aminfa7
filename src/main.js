cb3db52f66868b1a83a805e9d2e89c98fbeacb63
Amin Fazeli <aminfazeli787@yahoo.com>
2019-07-04 22:22:02 +0700

    finish part 1 :)

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ const form = document.forms[0];
 const monthly = form.elements.plan[1];
 monthly.addEventListener('mousedown', function() {
   document.querySelector('.form__total').textContent = '30$';
+  document.querySelector('.form__pay span').textContent = '30$';
 });
 const annually = form.elements.plan[0];
 annually.addEventListener('mousedown', function() {
   document.querySelector('.form__total').textContent = '300$';
+  document.querySelector('.form__pay span').textContent = '300$';
 });
 
 // enable button
