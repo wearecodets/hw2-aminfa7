67493c6955c1377a52bb89b7193070198789a151
Amin Fazeli <aminfazeli787@yahoo.com>
2019-07-04 19:53:58 +0700

    unfinish feature-accordion

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-console.log('Hello, world!');
+const form = document.forms[0];
+form.elements.plan.forEach(function(plan) {
+  plan.addEventListener('change', function() {
+    document.querySelector('.form__total').textContent = '30$';
+  });
+});
+
+const monthly = form.elements.plan[1];
+const annually = form.elements.plan[0];
+
+// enable button
+document.querySelector('.form__pay').disabled = false;
+
+// feature-accordition
+
+// please check line 20 !!!!
+const question = document.querySelectorAll('.accordion__question');
+question.forEach(function(add) {
+  add.addEventListener('click', function() {
+    add.classList.toggle('accordion--active');
+  });
+});
