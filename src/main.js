298bb5065292ecafa46c06603149b62ec4dd6445
Amin Fazeli <aminfazeli787@yahoo.com>
2019-07-04 22:09:16 +0700

    finish payment total

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
-// have not done the last part yet... 
-//is it like i have to choose the radios one by one and then the event will be mousedown terus textcontent to the right amount ??????
-
 const form = document.forms[0];
-form.elements.plan.forEach(function(plan) {
-  plan.addEventListener('change', function() {
-    document.querySelector('.form__total').textContent = '30$';
-  });
-});
-
 const monthly = form.elements.plan[1];
+monthly.addEventListener('mousedown', function() {
+  document.querySelector('.form__total').textContent = '30$';
+});
 const annually = form.elements.plan[0];
+annually.addEventListener('mousedown', function() {
+  document.querySelector('.form__total').textContent = '300$';
+});
 
 // enable button
 document.querySelector('.form__pay').disabled = false;
