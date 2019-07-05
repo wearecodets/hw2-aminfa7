2bf1a96a8af8ebcdd28530b4c4de4dbed70513bc
Amin Fazeli <aminfazeli787@yahoo.com>
2019-07-05 14:22:22 +0700

    need more help in payPal part

--- src/main.js
+++ src/main.js
@@ -1,25 +1,53 @@
+// something cool i learned :
+// before i chose mousedown n click as an event so if i click on the value the chosen input changes but the function doesnt work because the event is mouse down it must exactly be click on the radio button..... 
+
 const form = document.forms[0];
-const monthly = form.elements.plan[1];
-monthly.addEventListener('mousedown', function() {
-  document.querySelector('.form__total').textContent = '30$';
-  document.querySelector('.form__pay span').textContent = '30$';
-});
-const annually = form.elements.plan[0];
-annually.addEventListener('mousedown', function() {
-  document.querySelector('.form__total').textContent = '300$';
-  document.querySelector('.form__pay span').textContent = '300$';
+const payment = form.elements.plan;
+
+payment.forEach( function (pembayaran){ 
+  pembayaran.addEventListener('change',function(){
+     if (form.plan.value === 'Monthly') {
+    document.querySelector('.form__total').textContent = '30$';
+    document.querySelector('.form__pay span').textContent = '30$';};
+
+    if (form.plan.value === 'Annually') {
+    document.querySelector('.form__total').textContent = '300$';
+    document.querySelector('.form__pay span').textContent = '300$';}
+
+  });
 });
 
-// enable button
-document.querySelector('.form__pay').disabled = false;
+
+// enable button !!!!!!
+const payButton = form.elements.submit
+const money = document.querySelector('.form__pay span');
+ if (money.textContent !='$0') {
+   payButton.disabled = false;
+ }
+
+// payPAl Amount Bug
+//  need help in this :(((
+
+let payPal = document.querySelector     ('.ppaf-input');
+ payPal.setAttribute('value', money.textContent)
+
 
 // feature-accordition
 
-// please check line 23 !!!!
-// need a bit help for the last part
-const question = document.querySelectorAll('.accordion__question');
-question.forEach(function(add) {
+const questions = document.querySelectorAll('.accordion');
+
+questions.forEach(function(add) {
   add.addEventListener('click', function() {
     add.classList.toggle('accordion--active');
-  });
+   });
 });
+
+// please check these lines !!
+
+const questionOne = document.querySelector('.accordion');
+const questionTwo = document.querySelector('.accordion:last-child');
+
+//if (questionOne.classList = 'accordion--active') { questionTwo.classList.remove('accordion--active'); }
+//if (questionTwo.classList = 'accordion--active') { questionOne.classList.remove('accordion--active');}
+
+
