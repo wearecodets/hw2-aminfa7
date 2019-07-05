02739e13014acd354584fb7f8268aeb9ec5879fa
Amin Fazeli <aminfazeli787@yahoo.com>
2019-07-05 18:25:25 +0700

    fix payPal

--- src/main.js
+++ src/main.js
@@ -1,36 +1,35 @@
 // something cool i learned :
-// before i chose mousedown n click as an event so if i click on the value the chosen input changes but the function doesnt work because the event is mouse down it must exactly be click on the radio button..... 
+// before i chose mousedown n click as an event so if i click on the value the chosen input changes but the function doesnt work because the event is mouse down it must exactly be click on the radio button.....
 
 const form = document.forms[0];
 const payment = form.elements.plan;
 
-payment.forEach( function (pembayaran){ 
-  pembayaran.addEventListener('change',function(){
-     if (form.plan.value === 'Monthly') {
-    document.querySelector('.form__total').textContent = '30$';
-    document.querySelector('.form__pay span').textContent = '30$';};
+payment.forEach(function(pembayaran) {
+  pembayaran.addEventListener('change', function() {
+    if (form.plan.value === 'Monthly') {
+      document.querySelector('.form__total').textContent = '30$';
+      document.querySelector('.form__pay span').textContent = '30$';
+    }
 
     if (form.plan.value === 'Annually') {
-    document.querySelector('.form__total').textContent = '300$';
-    document.querySelector('.form__pay span').textContent = '300$';}
-
+      document.querySelector('.form__total').textContent = '300$';
+      document.querySelector('.form__pay span').textContent = '300$';
+    }
   });
 });
 
-
 // enable button !!!!!!
-const payButton = form.elements.submit
+const payButton = form.elements.submit;
 const money = document.querySelector('.form__pay span');
- if (money.textContent !='$0') {
-   payButton.disabled = false;
- }
+if (money.textContent != '$0') {
+  payButton.disabled = false;
+}
 
 // payPAl Amount Bug
 //  need help in this :(((
 
-let payPal = document.querySelector     ('.ppaf-input');
- payPal.setAttribute('value', money.textContent)
-
+const paypalLink = document.querySelector('a.form__cc-heading');
+paypalLink.setAttribute('href', `https://paypal.me/nikosolihin/${money}`);
 
 // feature-accordition
 
@@ -39,15 +38,11 @@ const questions = document.querySelectorAll('.accordion');
 questions.forEach(function(add) {
   add.addEventListener('click', function() {
     add.classList.toggle('accordion--active');
-   });
+  });
 });
 
-// please check these lines !!
-
-const questionOne = document.querySelector('.accordion');
+/* const questionOne = document.querySelector('.accordion');
 const questionTwo = document.querySelector('.accordion:last-child');
 
 //if (questionOne.classList = 'accordion--active') { questionTwo.classList.remove('accordion--active'); }
-//if (questionTwo.classList = 'accordion--active') { questionOne.classList.remove('accordion--active');}
-
-
+//if (questionTwo.classList = 'accordion--active') { questionOne.classList.remove('accordion--active');} */
